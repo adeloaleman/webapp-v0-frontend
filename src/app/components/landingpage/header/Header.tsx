fc4da6c30e5104afb5fd434a1836275159964a28
adeloaleman <adeloaleman@gmail.com>
2025-07-13 21:41:37 +0100

    update

--- src/app/components/landingpage/header/Header.tsx
+++ src/app/components/landingpage/header/Header.tsx
@@ -48,9 +48,9 @@ const LpHeader = () => {
     <AppBarStyled position="sticky" elevation={8}>
       <Container maxWidth="lg">
         <ToolbarStyled>
-          <Box position="relative" top="13px">
-            <Logo />
-          </Box>
+          {/* <Box position="relative" top="13px"> */}
+          <Logo />
+          {/* </Box> */}
           <Box flexGrow={1} />
           {lgDown ? (
             <IconButton
